2679f2f0a14837d8e79e5d70726baf272b4fdf62
Nathan Prouse <nprouse1441@gmail.com>
2019-08-19 20:53:30 +0000

    autopep8

--- filthify.py
+++ filthify.py
@@ -2,48 +2,49 @@ from image_resrc import *
 from configengine import *
 import sys
 
-prefConfig="prefs.json"
-hardConfig="config.json"
-vulnsConfig="vulns.json"
+prefConfig = "prefs.json"
+hardConfig = "config.json"
+vulnsConfig = "vulns.json"
+
 
 def main():
-   Configurer=ConfigEngine(hardConfig,prefConfig)
-   vm=Image(Configurer)
-   #print len(vm.insertions),vm.engine.getNumVulns()
-   vm.makeInitFile()
-   vm.makeScoreFile()
-   vm.makeScenarioFile()
-   beginTemplate="""
+    Configurer = ConfigEngine(hardConfig, prefConfig)
+    vm = Image(Configurer)
+    vm.makeInitFile()
+    vm.makeScoreFile()
+    vm.makeScenarioFile()
+    beginTemplate = """
 {
 	"teams":{
 		"test": "Test Team"
 	},
 	"linux":{
    """
-   midTemplate="""
+    midTemplate = """
 		"{num}":["{description}",{value},"{boolean}"],
    """
-   endTemplate="""
+    endTemplate = """
 	}
 }
    """
 
-   for i,j in enumerate(vm.getInsertions()):
-      if i+1==len(vm.getInsertions()):
-         midTemplate=midTemplate[:-5]
-      beginTemplate+=midTemplate.format(
-         **{
-            "num":i+1,
-            "description":j.getDescription(),
-            "value":0,
-            "boolean":j.getScoreCmd()
-         }
-      )
-   beginTemplate+=endTemplate
+    for i, j in enumerate(vm.getInsertions()):
+        if i+1 == len(vm.getInsertions()):
+            midTemplate = midTemplate[:-5]
+        beginTemplate += midTemplate.format(
+            **{
+                "num": i+1,
+                "description": j.getDescription(),
+                "value": 0,
+                "boolean": j.getScoreCmd()
+            }
+        )
+    beginTemplate += endTemplate
 
-   with open("build/server.json","w") as f:
-      f.write(beginTemplate)
-      f.close()
-if __name__=="__main__":
-   main()
+    with open("build/server.json", "w") as f:
+        f.write(beginTemplate)
+        f.close()
 
+
+if __name__ == "__main__":
+    main()
--- image_resrc.py
+++ image_resrc.py
@@ -3,84 +3,81 @@ import random
 import os
 import string
 
+
 class User(object):
-   def __init__(self,name,password="password",groups=[],hidden=False,strength="strong",authorized=True):
-      self.username=name
-      self.password=password
-      self.groups=groups
-      self.isHidden=hidden
-      if strength=="weak":
-         self.encryption="md5"
-      else:
-         self.encryption="sha512"
-      self.authorized=authorized
+    def __init__(self, name, password="password", groups=[], hidden=False, strength="strong", authorized=True):
+        self.username = name
+        self.password = password
+        self.groups = groups
+        self.isHidden = hidden
+        if strength == "weak":
+            self.encryption = "md5"
+        else:
+            self.encryption = "sha512"
+        self.authorized = authorized
 
-   def getInitCmd(self):
-      if self.isHidden:
-         args="-r -M -N -o "
-      else:
-         args="-m "
-      if len(self.groups)>0:
-         args+="-G "
-         for i in self.groups:
-            args+=i+","
-         if args[-1]==",":args=args[:-1]
-      adduser="useradd %s -s '/bin/bash' -p $(mkpasswd --method=%s %s) %s" % (self.username,self.encryption,self.password,args)
-      return adduser
-
-class Insertion(object): #abstract class for any change made to image.
-   def __init__(self,conf): #conf is the array for the insertion in the vulns.json file
-      self.description=conf[0]
-      self.scoringBoolean=conf[1]
-      self.initCommand=conf[2]
-      self.dependencies=conf[-1]
-#      if len(conf)==4: self.arguments=conf[3]
-
-   def getScoreCmd(self):
-      return self.scoringBoolean
-
-   def getDescription(self):
-      return self.description
-
-   def getInitCmd(self):
-      return self.initCommand
-
-   def initialize(self):
-      os.system(initCommand)
-
-class Vuln(Insertion): #(probably an subclass of Insertion) a vulnerability
-   pass
-
-class Service(Insertion): #("") a service
-   def __init__(self,conf):
-      #print conf
-      super(Service,self).__init__(conf)
+    def getInitCmd(self):
+        if self.isHidden:
+            args = "-r -M -N -o "
+        else:
+            args = "-m "
+        if len(self.groups) > 0:
+            args += "-G "
+            for i in self.groups:
+                args += i+","
+            if args[-1] == ",":
+                args = args[:-1]
+        adduser = "useradd %s -s '/bin/bash' -p $(mkpasswd --method=%s %s) %s" % (
+            self.username, self.encryption, self.password, args)
+        return adduser
 
 
-class Package(Insertion): #("") a package
-   pass
+class Insertion(object):  # abstract class for any change made to image.
+    def __init__(self, conf):  # conf is the array for the insertion in the vulns.json file
+        self.description = conf[0]
+        self.scoringBoolean = conf[1]
+        self.initCommand = conf[2]
+        self.dependencies = conf[-1]
 
-class Image(): #a virtual machine image
-   def __init__(self,configEngine):
-      self.engine=configEngine
+    def getScoreCmd(self):
+        return self.scoringBoolean
 
-      self.numVulns=self.engine.getNumVulns()
-      self.distro=self.engine.getDistro()
-      self.difficulty=self.engine.getDifficulty()
-      self.reqServices=self.engine.getReqServices()
-      self.services=[]
-      self.insertions=[]
-      self.users=[]
-      self.adminUsers=[]
-      self.mainUser=""
-      self.initFile="build/initfile.bash"
-      self.booleanFile="build/scoreconfig.json"
-      self.dependencies=[]
-      if "random" in self.engine.getVulnerability():
-         self.initAllRandom()
-      else:
-         self.initImage()
-      self.scenario="""
+    def getDescription(self):
+        return self.description
+
+    def getInitCmd(self):
+        return self.initCommand
+
+    def initialize(self):
+        os.system(initCommand)
+
+
+
+class Service(Insertion):
+    def __init__(self, conf):
+        super(Service, self).__init__(conf)
+
+class Image():  # a virtual machine image
+    def __init__(self, configEngine):
+        self.engine = configEngine
+
+        self.numVulns = self.engine.getNumVulns()
+        self.distro = self.engine.getDistro()
+        self.difficulty = self.engine.getDifficulty()
+        self.reqServices = self.engine.getReqServices()
+        self.services = []
+        self.insertions = []
+        self.users = []
+        self.adminUsers = []
+        self.mainUser = ""
+        self.initFile = "build/initfile.bash"
+        self.booleanFile = "build/scoreconfig.json"
+        self.dependencies = []
+        if "random" in self.engine.getVulnerability():
+            self.initAllRandom()
+        else:
+            self.initImage()
+        self.scenario = """
 
 <Insert Default Scenario Template Here>
 
@@ -94,152 +91,156 @@ Authorized Users:
 {users}
 """
 
-
-   def initImage(self):
-      vulns=self.engine.getVulnerability()
-      for i in self.engine.formatVulns(vulns):
-         tmp=Insertion(i)
-         self.insertions.append(tmp)
-         self.dependencies.append(tmp.dependencies)
-      self.initUsers()
-      self.initServices()
+    def initImage(self):
+        vulns = self.engine.getVulnerability()
+        for i in self.engine.formatVulns(vulns):
+            tmp = Insertion(i)
+            self.insertions.append(tmp)
+            self.dependencies.append(tmp.dependencies)
+        self.initUsers()
+        self.initServices()
 #      self.initDependencies()
 
-   def initAllRandom(self):
-      self.initUsers()
-      self.initServices()
-      self.initServiceVulns()
-      self.initCategoryVulns()
-      #self.initDependencies()
+    def initAllRandom(self):
+        self.initUsers()
+        self.initServices()
+        self.initServiceVulns()
+        self.initCategoryVulns()
+        # self.initDependencies()
 
-   def initCategoryVulns(self):
-      weights=self.engine.getCatWeights()
-      weights.pop('services')
-      tempUsers=self.users
-      for i in weights.keys():
-         for j in self.engine.getMasterConfig()['config']['validDiffs']:
-           #print i,j
-            if self.engine.getVulnCountForCategory(i,j)>0:
-               #print self.engine.getVulnCountForCategory(i,j)
-               for k in self.engine.getRandVulns(i,j,self.engine.getVulnCountForCategory(i,j)):
-                  if any("{randomUser}" in l for l in k):
-                     u=random.choice(tempUsers)
-                     tempUsers.remove(u)
-                     context={"randomUser":u.username}
-                     k=[line.format(**context) if "{randomUser}" else line in line for line in k]
-                  if any("{mainUser}" in l for l in k):
-                     k=[line.format(**{"mainUser":self.mainUser}) if "{mainUser}" else line in line for line in k]
-                  if any("{randomUsername}" in l for l in k):
-                     name=random.choice(self.wordlist)
-                     self.wordlist.remove(name)
-                     k=[line.format(**{"randomUsername":name}) if "{randomUserName}" in line else line for line in k]
-                  #print k
-                  tmp=Insertion(k)
-                  self.insertions.append(tmp)
-                  self.dependencies.append(tmp.dependencies)
-   def initDependencies(self):
-      #filelist = [ f for f in os.listdir("./archive/") ]
-      #for f in filelist:
-      #   os.remove(os.path.join("./archive/", f))
-      with tarfile.open("./build/dependencies.tar.gz", "w:gz") as tar:
-         for i in self.dependencies:
-            #print i
-            tar.add(i)
-   def initServiceVulns(self):
-      for i in self.reqServices:
-         for j in self.engine.getMasterConfig()['config']['validDiffs']:
-            if self.engine.getVulnCountForService(i,j)>0:
-               #print self.engine.getVulnCountForService(i,j)
-               for k in self.engine.getRandVulns(i,j,self.engine.getVulnCountForService(i,j)):
-                  self.insertions.append(Insertion(k))
+    def initCategoryVulns(self):
+        weights = self.engine.getCatWeights()
+        weights.pop('services')
+        tempUsers = self.users
+        for i in weights.keys():
+            for j in self.engine.getMasterConfig()['config']['validDiffs']:
+                if self.engine.getVulnCountForCategory(i, j) > 0:
+                    for k in self.engine.getRandVulns(i, j, self.engine.getVulnCountForCategory(i, j)):
+                        if any("{randomUser}" in l for l in k):
+                            u = random.choice(tempUsers)
+                            tempUsers.remove(u)
+                            context = {"randomUser": u.username}
+                            k = [line.format(
+                                **context) if "{randomUser}" else line in line for line in k]
+                        if any("{mainUser}" in l for l in k):
+                            k = [line.format(
+                                **{"mainUser": self.mainUser}) if "{mainUser}" else line in line for line in k]
+                        if any("{randomUsername}" in l for l in k):
+                            name = random.choice(self.wordlist)
+                            self.wordlist.remove(name)
+                            k = [line.format(
+                                **{"randomUsername": name}) if "{randomUserName}" in line else line for line in k]
+                        tmp = Insertion(k)
+                        self.insertions.append(tmp)
+                        self.dependencies.append(tmp.dependencies)
 
-   def initUsers(self):
-      self.wordlist=[line.rstrip('\n') for line in open('names.txt')]
-      characters=string.ascii_letters+string.digits
-      for i in range(int(round(self.engine.getUserCount()*.8))):
-         name=random.choice(self.wordlist)
-         self.wordlist.remove(name)
-         password=''.join(random.choice(characters) for j in range(random.randint(6,12)))
-         groups=[]
-         hidden=False
-         strength="strong"
-         self.users.append(User(name,password,groups,hidden,strength))
-      for i in range(int(round(self.engine.getUserCount()*.2))):
-         name=random.choice(self.wordlist)
-         self.wordlist.remove(name)
-         password=''.join(random.choice(characters) for j in range(random.randint(6,12)))
-         groups=['sudo']
-         hidden=False
-         strength="strong"
-         u=(User(name,password,groups,hidden,strength))
-         self.users.append(u)
-         self.adminUsers.append(u)
-      self.mainUser=random.choice(self.adminUsers)
+    def initDependencies(self):
+        with tarfile.open("./build/dependencies.tar.gz", "w:gz") as tar:
+            for i in self.dependencies:
+                tar.add(i)
 
-   def initServices(self):
-      for i in self.reqServices:
-         self.services.append(Service(self.engine.getService(i)))
+    def initServiceVulns(self):
+        for i in self.reqServices:
+            for j in self.engine.getMasterConfig()['config']['validDiffs']:
+                if self.engine.getVulnCountForService(i, j) > 0:
+                    for k in self.engine.getRandVulns(i, j, self.engine.getVulnCountForService(i, j)):
+                        self.insertions.append(Insertion(k))
 
-   def getInsertions(self):
-      return self.insertions
+    def initUsers(self):
+        self.wordlist = [line.rstrip('\n') for line in open('names.txt')]
+        characters = string.ascii_letters+string.digits
+        for i in range(int(round(self.engine.getUserCount()*.8))):
+            name = random.choice(self.wordlist)
+            self.wordlist.remove(name)
+            password = ''.join(random.choice(characters)
+                               for j in range(random.randint(6, 12)))
+            groups = []
+            hidden = False
+            strength = "strong"
+            self.users.append(User(name, password, groups, hidden, strength))
+        for i in range(int(round(self.engine.getUserCount()*.2))):
+            name = random.choice(self.wordlist)
+            self.wordlist.remove(name)
+            password = ''.join(random.choice(characters)
+                               for j in range(random.randint(6, 12)))
+            groups = ['sudo']
+            hidden = False
+            strength = "strong"
+            u = (User(name, password, groups, hidden, strength))
+            self.users.append(u)
+            self.adminUsers.append(u)
+        self.mainUser = random.choice(self.adminUsers)
 
-   def makeInitFile(self):
-      with open(self.initFile,"w") as f:
-         f.write("""#!/bin/bash\n""")
-         for i in self.users:
-            f.write(i.getInitCmd())
-            f.write("\n")
-         for i in self.services:
-            f.write(i.getInitCmd())
-            f.write("\n")
-         for i in self.insertions:
-            f.write(i.getInitCmd())
-            f.write("\n")
+    def initServices(self):
+        for i in self.reqServices:
+            self.services.append(Service(self.engine.getService(i)))
 
-      return f
+    def getInsertions(self):
+        return self.insertions
 
-   def makeScoreFile(self):
-      with open(self.booleanFile,"w") as f:
-         f.write("{\n")
-         for j,i in enumerate(self.services):
-            end=",\n"
-            if j==len(self.services)-1 and len(self.insertions)==0: end="\n"
-            f.write("\""+i.description+"\""+":"+"\""+i.getScoreCmd().replace("\n",'')+"\""+end)
-         for j,i in enumerate(self.insertions):
-            end=",\n"
-            if j==len(self.insertions)-1: end="\n"
-            f.write("\""+i.description+"\""+":"+"\""+i.getScoreCmd().replace("\n",'')+"\""+end)
-         f.write("}\n")
-   def makeScenarioFile(self):
-      sTemp="""
+    def makeInitFile(self):
+        with open(self.initFile, "w") as f:
+            f.write("""#!/bin/bash\n""")
+            for i in self.users:
+                f.write(i.getInitCmd())
+                f.write("\n")
+            for i in self.services:
+                f.write(i.getInitCmd())
+                f.write("\n")
+            for i in self.insertions:
+                f.write(i.getInitCmd())
+                f.write("\n")
+
+        return f
+
+    def makeScoreFile(self):
+        with open(self.booleanFile, "w") as f:
+            f.write("{\n")
+            for j, i in enumerate(self.services):
+                end = ",\n"
+                if j == len(self.services)-1 and len(self.insertions) == 0:
+                    end = "\n"
+                f.write("\""+i.description+"\""+":"+"\"" +
+                        i.getScoreCmd().replace("\n", '')+"\""+end)
+            for j, i in enumerate(self.insertions):
+                end = ",\n"
+                if j == len(self.insertions)-1:
+                    end = "\n"
+                f.write("\""+i.description+"\""+":"+"\"" +
+                        i.getScoreCmd().replace("\n", '')+"\""+end)
+            f.write("}\n")
+
+    def makeScenarioFile(self):
+        sTemp = """
 """
-      aTemp="""
+        aTemp = """
 """
-      uTemp="""
+        uTemp = """
 """
-      for i in self.services:
-         sTemp+=(i.description+"\n")
-      aTemp+=self.mainUser.username+":"+self.mainUser.password+" (you)\n"
-      tmp=self.adminUsers
-      tmp.remove(self.mainUser)
-      for i in tmp:
-         aTemp+=(i.username+":"+i.password+"\n")
-      for i in (list(set(self.users)-set(self.adminUsers))):
-         uTemp+=(i.username+"\n")
-      self.scenario=self.scenario.format(**
-         {
-            "services":sTemp,
-            "admins":aTemp,
-            "users":uTemp
-         }
-      )
-      with open("build/scenario.txt",'w') as f:
-         f.write(self.scenario)
+        for i in self.services:
+            sTemp += (i.description+"\n")
+        aTemp += self.mainUser.username+":"+self.mainUser.password+" (you)\n"
+        tmp = self.adminUsers
+        tmp.remove(self.mainUser)
+        for i in tmp:
+            aTemp += (i.username+":"+i.password+"\n")
+        for i in (list(set(self.users)-set(self.adminUsers))):
+            uTemp += (i.username+"\n")
+        self.scenario = self.scenario.format(**
+                                             {
+                                                 "services": sTemp,
+                                                 "admins": aTemp,
+                                                 "users": uTemp
+                                             }
+                                             )
+        with open("build/scenario.txt", 'w') as f:
+            f.write(self.scenario)
 
-   def recursiveFormat(self,array,dict):
-      for element in array:
-         if type(element) is str:
-            array[array.index(element)]=element.format(**dict)
-         else:
-            array[array.index(element)]=self.recursiveFormat(element,dict)
-      return array
+    def recursiveFormat(self, array, dict):
+        for element in array:
+            if type(element) is str:
+                array[array.index(element)] = element.format(**dict)
+            else:
+                array[array.index(element)] = self.recursiveFormat(
+                    element, dict)
+        return array
